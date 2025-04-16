353fcd60600157d86048e96e5336637839e5fa4e
Kristian Lubina <kristian.lubina@outlook.com>
2025-04-16 10:07:13 +0200

    Add files via upload

---
+++ ConsoleAppIndexableCollections/Program.cs
@@ -0,0 +1,23 @@
+﻿//from here
+https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/collections#indexable-collections
+
+//create a list
+List<string> salmons = [ "chinook", "coho", "pink", "sockeye" ];
+
+//add a new element
+salmons.Add( "new" );
+
+//iterate using the index
+for ( var index = 0; index < salmons.Count; index++)
+  {
+  Console.WriteLine( salmons[ index ] + " " );
+  }
+
+/* 
+ * Output:
+ * chinook
+ * coho
+ * pink
+ * sockeye
+ * new
+ */
